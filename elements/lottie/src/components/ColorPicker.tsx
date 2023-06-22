c87504890fd0bba2773cca5de4e26933ddf92edd
Bård N. Hovde <bnhovde@gmail.com>
2023-06-22 13:53:43 +0200

    Added Lottie-cli component v1

---
+++ elements/lottie/src/components/ColorPicker.tsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from "react";
+import { getColors } from "lottie-colorify";
+import { SchemaFieldProps, SchemaFieldTypes } from "@vev/react";
+import { LottieColor, LottieColorReplacement } from "../types";
+import defaultAnimation from "../constants/defaultAnimation";
+import sortLottieColors from "../utils/sortLottieColors";
+import {
+  SilkeBox,
+  SilkeColorPickerButton,
+  SilkeOverflowMenu,
+  SilkeText,
+  SilkeTextField,
+} from "@vev/silke";
+
+const ColorPicker = ({
+  context,
+  value = [],
+  onChange,
+}: SchemaFieldProps<SchemaFieldTypes["array"]>) => {
+  const [json, setJson] = useState();
+  const [lottieColors, setLottieColors] = useState<LottieColor[]>([]);
+  const filePath = context.value?.file?.url || defaultAnimation;
+
+  // Fetch json data when file url changes
+  useEffect(() => {
+    const fetchJson = async () => {
+      try {
+        const response = await fetch(filePath);
+        if (response.ok) {
+          const result = await response.json();
+          setJson(result);
+        }
+      } catch (e) {
+        setJson(undefined);
+      }
+    };
+
+    fetchJson();
+  }, [filePath]);
+
+  // Get colors from json
+  useEffect(() => {
+    const getLottieColors = () => {
+      try {
+        const result = getColors(json) as LottieColor[];
+
+        const uniqueResult = Array.from(
+          new Map(result.map((c) => [`${c.join()}`, c])).values()
+        );
+        const sortedResult = sortLottieColors(uniqueResult);
+
+        setLottieColors(sortedResult);
+      } catch (e) {
+        setLottieColors([]);
+      }
+    };
+
+    getLottieColors();
+  }, [json]);
+
+  // Handle color input
+  const onChangeColor = (oldColor: LottieColor, newColor: string) => {
+    // Filter out existing change if same color
+    const newColors = (value ? [...value] : []).filter((oldChange) => {
+      const isSameCol = String(oldChange.oldColor) === String(oldColor);
+
+      return !isSameCol;
+    }) as LottieColorReplacement[];
+
+    // Lottie crashes on short hex values like #fff
+    if (newColor.length < 7) {
+      newColor = `${newColor}${newColor.substring(1)}`;
+    }
+
+    newColors.push({ oldColor, newColor });
+    onChange(newColors);
+  };
+
+  const onReset = () => {
+    onChange([]);
+  };
+
+  return (
+    <SilkeBox column flex gap="s">
+      <SilkeBox flex gap="m" hAlign="spread">
+        <SilkeText tag="label">Colors</SilkeText>
+        <SilkeOverflowMenu
+          icon="menuMore"
+          kind="ghost"
+          size="s"
+          items={[
+            {
+              label: `Reset colors`,
+              value: "reset",
+              onClick: () => onReset(),
+            },
+          ]}
+        />
+      </SilkeBox>
+      <SilkeBox column flex gap="m">
+        {lottieColors?.map((color, index) => {
+          const replacement = value.find(
+            (v) => String(v.oldColor) === String(color)
+          ) as LottieColorReplacement;
+
+          const rgb = replacement
+            ? replacement.newColor
+            : `rgb(${color[0]},${color[1]},${color[2]})`;
+
+          return (
+            <SilkeBox key={`${index}-${color}`} vAlign="center" flex gap="xs">
+              <SilkeColorPickerButton
+                value={rgb}
+                kind="ghost"
+                size="s"
+                noVariables
+                onChange={(newColor) => onChangeColor(color, newColor)}
+              />
+              <SilkeTextField
+                value={rgb}
+                onChange={(newColor: string) => onChangeColor(color, newColor)}
+              />
+            </SilkeBox>
+          );
+        })}
+      </SilkeBox>
+    </SilkeBox>
+  );
+};
+
+export default ColorPicker;
