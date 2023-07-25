374f3106b73b13be6303b719263fa759956211dd
Audun Øygard <audun@vev.design>
2023-07-25 09:38:46 +0200

    Move Spline to CLI

---
+++ elements/spline/src/Spline.tsx
@@ -0,0 +1,43 @@
+import React, { useRef } from 'react';
+import styles from './Spline.module.css';
+import { registerVevComponent } from '@vev/react';
+
+type Props = {
+  formUrl: string;
+  hostRef: React.RefObject<HTMLDivElement>;
+};
+
+const Spline = ({
+  formUrl = 'https://my.spline.design/carcampingphysicscopy-56fe6d4adf9415188037704bd4b6d775/',
+}: Props) => {
+  const frameRef = useRef<HTMLIFrameElement>(null);
+
+  if (formUrl === '') {
+    return (
+      <div className={styles.info}>
+        <h3>Double-click this Element to add your Spline URL</h3>
+      </div>
+    );
+  }
+
+  return (
+    <iframe
+      src={formUrl}
+      ref={frameRef}
+      className={styles.wrapper}
+      frameBorder={0}
+      marginHeight={0}
+      marginWidth={0}
+    >
+      Loading…
+    </iframe>
+  );
+};
+
+registerVevComponent(Spline, {
+  name: 'Spline',
+  props: [{ name: 'formUrl', title: 'Spline URL', type: 'string', initialValue: '' }],
+  type: 'both',
+});
+
+export default Spline;
