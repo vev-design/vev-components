986ef556093c944ca934b8accbfe51a6f90f1990
Audun Øygard <audun@vev.design>
2023-10-04 14:49:28 +0200

    Added events to spotify embed

---
+++ spotify/src/event-types.ts
@@ -0,0 +1,11 @@
+export enum EventTypes {
+  PLAYING = 'PLAYING',
+  PAUSED = 'PAUSED',
+}
+
+export enum InteractionTypes {
+  PLAY = 'PLAY',
+  PLAY_FROM_START = 'PLAY_FROM_START',
+  PAUSE = 'PAUSE',
+  TOGGLE = 'TOGGLE',
+}
