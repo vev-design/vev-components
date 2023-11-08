5cf59cc823b6e6e6aea6e297727a4fcbe5e1dd9c
Audun Øygard <audun@vev.design>
2023-11-08 13:31:03 +0100

    Clean up mail chimp a bit

--- mailchimp/src/util.ts
+++ mailchimp/src/util.ts
@@ -4,35 +4,6 @@ export function textToDom(html: string, ownerDocument: Document) {
   return dummyEl;
 }
 
-export function inlinePlaceholders(chimpForm: HTMLDivElement) {
-  const labels = chimpForm.querySelectorAll('label');
-  for (const label in labels) {
-    const labelToInline = labels[label];
-    if (labelToInline && labelToInline.attributes && labelToInline.getAttribute('for')) {
-      const input = chimpForm.querySelector(
-        '#' + labelToInline.getAttribute('for'),
-      ) as HTMLInputElement;
-
-      if (input && input.type !== 'radio' && !(parseInt(input.getAttribute('maxlength')) < 10)) {
-        if (input.tagName === 'SELECT') {
-          input.querySelectorAll('option').forEach((option) => {
-            if (option.value === '') {
-              option.innerText = labelToInline.innerText;
-              labelToInline.style.display = 'none';
-            } else {
-              labelToInline.style.display = 'none';
-            }
-          });
-        } else {
-          input.setAttribute('placeholder', labelToInline.innerText);
-          labelToInline.style.display = 'none';
-        }
-      }
-    }
-  }
-  return chimpForm;
-}
-
 export function addVevClasses(chimpForm: HTMLDivElement) {
   const inputs = chimpForm.querySelectorAll(
     'input[type="number"],input[type="text"],input[type="url"],input[type="email"],select',
