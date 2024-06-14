2e8f5bbdc5b5cf70dc483fc29b40afefd92b5c7b
Audun Øygard <audun@vev.design>
2024-06-14 07:12:44 +0200

    Formatting

--- instagram/src/Instagram.tsx
+++ instagram/src/Instagram.tsx
@@ -1,42 +1,37 @@
-import React from "react";
-import styles from "./Instagram.module.css";
-import { registerVevComponent } from "@vev/react";
+import React from 'react';
+import styles from './Instagram.module.css';
+import { registerVevComponent } from '@vev/react';
 
-const INSTA_PREFIX = "https://www.instagram.com/p/";
-const INSTA_POSTFIX = "/embed";
-const INSTA_CAPTION = "/captioned";
+const INSTA_PREFIX = 'https://www.instagram.com/p/';
+const INSTA_POSTFIX = '/embed';
+const INSTA_CAPTION = '/captioned';
 
 function buildUrl(id, includeCaptions) {
-  return (
-    INSTA_PREFIX +
-    stripUrl(id) +
-    INSTA_POSTFIX +
-    (includeCaptions ? INSTA_CAPTION : "")
-  );
+  return INSTA_PREFIX + stripUrl(id) + INSTA_POSTFIX + (includeCaptions ? INSTA_CAPTION : '');
 }
 
 function stripUrl(url) {
-  if (url.indexOf("/") === -1) {
+  if (url.indexOf('/') === -1) {
     // most likely just image id
     return url;
   }
 
-  if (url.indexOf("<") !== -1) {
+  if (url.indexOf('<') !== -1) {
     // trying find instagram url and match until end quote (")
     const matches = url.match(/instagram.com\/p\/.+?(")/);
-    if (!matches.length) return "";
+    if (!matches.length) return '';
     url = matches[0];
-    url = "https://" + url.substr(0, url.length - 2);
+    url = 'https://' + url.substr(0, url.length - 2);
   }
 
-  if (url.indexOf("instagram.com/p/") !== -1) {
+  if (url.indexOf('instagram.com/p/') !== -1) {
     // most likely whole url
 
     // remove all arguments
-    const i = url.indexOf("?");
+    const i = url.indexOf('?');
     if (i !== -1) url = url.substring(0, i);
 
-    const [, , stripped] = url.replace(/^http(s)?:\/\//, "").split("/");
+    const [, , stripped] = url.replace(/^http(s)?:\/\//, '').split('/');
 
     return stripped;
   }
@@ -50,7 +45,7 @@ type Props = {
 };
 
 const Instagram = ({
-  url = "https://www.instagram.com/p/CpAQZfDDseY",
+  url = 'https://www.instagram.com/p/CpAQZfDDseY',
   isCaptioned = false,
 }: Props) => {
   if (url) {
@@ -76,32 +71,32 @@ const Instagram = ({
 };
 
 registerVevComponent(Instagram, {
-  name: "Instagram",
-  description: "Add your Instagram post on your canvas.",
-  type: "standard",
+  name: 'Instagram',
+  description: 'Add your Instagram post on your canvas.',
+  type: 'standard',
   props: [
     {
-      title: "Instagram URL",
-      name: "url",
-      type: "string",
-      initialValue: "https://www.instagram.com/p/CpAQZfDDseY",
+      title: 'Instagram URL',
+      name: 'url',
+      type: 'string',
+      initialValue: 'https://www.instagram.com/p/CpAQZfDDseY',
       options: {
-        type: "text",
+        type: 'text',
         multiline: true,
       },
     },
     {
-      title: "Captions",
-      name: "isCaptioned",
-      type: "boolean",
+      title: 'Captions',
+      name: 'isCaptioned',
+      type: 'boolean',
       initialValue: false,
     },
   ],
   editableCSS: [
     {
       selector: styles.iframe,
-      title: "Container",
-      properties: ["border", "opacity", "filter"],
+      title: 'Container',
+      properties: ['border', 'opacity', 'filter'],
     },
   ],
 });
