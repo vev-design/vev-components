2566a05ad581edc3d59f4f73303e0e4eda6d7236
Audun Øygard <audun@vev.design>
2024-06-17 11:23:10 +0200

    Formatting

--- IFrame/src/IFrame.tsx
+++ IFrame/src/IFrame.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import styles from "./IFrame.module.css";
-import { registerVevComponent, useSize } from "@vev/react";
+import React from 'react';
+import styles from './IFrame.module.css';
+import { registerVevComponent, useSize } from '@vev/react';
 
 type Props = {
   pageUrl: string;
@@ -9,7 +9,7 @@ type Props = {
 };
 
 const getSrc = (src: string) => {
-  if (!/^https?:\/\//i.test(src)) return "https://" + src;
+  if (!/^https?:\/\//i.test(src)) return 'https://' + src;
   return src;
 };
 
@@ -38,38 +38,31 @@ const IFrame = ({ pageUrl, hostRef, scale = 0 }: Props) => {
 };
 
 registerVevComponent(IFrame, {
-  name: "IFrame",
+  name: 'IFrame',
   props: [
     {
-      name: "pageUrl",
-      title: "URL to page",
-      type: "string",
+      name: 'pageUrl',
+      title: 'URL to page',
+      type: 'string',
       description:
         "If the content you're trying to embed cannot be displayed the site might not allow embedding in an iframe",
     },
-    { name: "scale", title: "Scale page", type: "number", initialValue: 0 },
+    { name: 'scale', title: 'Scale page', type: 'number', initialValue: 0 },
   ],
   emptyState: {
-    action: "OPEN_PROPERTIES",
-    linkText: "Add URL",
-    description: "to page",
-    checkProperty: "pageUrl",
+    action: 'OPEN_PROPERTIES',
+    linkText: 'Add URL',
+    description: 'to page',
+    checkProperty: 'pageUrl',
   },
   editableCSS: [
     {
-      title: "Container",
+      title: 'Container',
       selector: styles.wrapper,
-      properties: [
-        "background",
-        "border-radius",
-        "border",
-        "filter",
-        "padding",
-        "margin",
-      ],
+      properties: ['background', 'border-radius', 'border', 'filter', 'padding', 'margin'],
     },
   ],
-  type: "both",
+  type: 'both',
 });
 
 export default IFrame;
