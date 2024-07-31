b61e0531e235f45f51f517615a39c43ae3f53fa2
Audun Øygard <audun@vev.design>
2024-07-31 13:55:55 +0200

    Switch to worse algo for opacity

--- object-3D/src/util/is-hotspot-visible.ts
+++ object-3D/src/util/is-hotspot-visible.ts
@@ -1,27 +1,16 @@
 import { CanvasHotspot } from '../hooks/use-hotspots';
-import { Camera, Group, Raycaster } from 'three';
+import { Camera, Group, Raycaster, Vector3 } from 'three';
 
 const raycaster = new Raycaster();
-let resultArr = [];
+const resultArr = [];
 
 export function isHotspotVisible(hotspot: CanvasHotspot, camera: Camera, scene: Group) {
-  resultArr = [];
-  const hotspotPos = hotspot.hotspot.position;
-  const direction = hotspotPos.clone().sub(camera.position);
-
-  raycaster.set(camera.position, direction.normalize());
-
-  let nearestInScene = Number.MAX_VALUE;
-  raycaster.intersectObject(scene, true, resultArr);
-  for (const intersectionIndex in resultArr) {
-    if (resultArr[intersectionIndex].distance < nearestInScene)
-      nearestInScene = resultArr[intersectionIndex].distance;
-  }
-  resultArr = [];
-  const intersectSphere = raycaster.intersectObject(hotspot.intersectionSphere, false, resultArr);
-  if (intersectSphere[0].distance < nearestInScene) {
+  const target = hotspot.hotspot;
+  const normalVector = new Vector3(target.position.x, target.position.y, target.position.z);
+  const camVector = new Vector3(0, 0, -1).applyQuaternion(camera.quaternion);
+  if (normalVector.angleTo(camVector) > Math.PI / 2) {
     hotspot.element.style.opacity = '1';
   } else {
-    hotspot.element.style.opacity = '0.2';
+    hotspot.element.style.opacity = '0.1';
   }
 }
